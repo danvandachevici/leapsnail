601dc5c18d2fde27db1a19e804e29e25a942dc8c
Dan VANDACHEVICI <dan@vandachevici.ro>
2022-06-13 10:14:02 +0200

    Added netflix

--- src/urls-map.ts
+++ src/urls-map.ts
@@ -1,7 +1,9 @@
 const urls = {
   g: (query: string, tokens: string[]): string =>
-    'https://google.com/search?q=' + tokens.join(' '),
+    'https://google.com/search?q=' + encodeURIComponent(tokens.join(' ')),
   yt: (query: string, tokens: string[]): string =>
-    'https://www.youtube.com/results?search_query=' + tokens.join(' '),
+    'https://www.youtube.com/results?search_query=' + encodeURIComponent(tokens.join(' ')),
+  n: (query: string, tokens: string[]): string => 
+    'https://www.netflix.com/search?q=' + encodeURIComponent(tokens.join(' ')),
 };
 export default urls;
